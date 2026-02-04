f399c01a5c934f0914923a2701b36ca4a3c61b34
c0mplex <sadasipa@gmail.com>
2026-02-04 15:20:38 -0500

    adding smart ^j and ^k search and autocomplete

--- .config/zsh/keybinds.zsh
+++ .config/zsh/keybinds.zsh
@@ -1,33 +1,66 @@
-##
-## Keybindings
-##
+## =============== ##
+##   Keybindings   ##
+## =============== ##
+
 bindkey -v
-bindkey '^[[H' beginning-of-line
-bindkey '^[[F' end-of-line
-#bindkey -s '^K' 'ls^M'
-bindkey -s '^o' '_smooth_fzf^M'
+
+function smart-k-up() {
+    if [[ -n "$POSTDISPLAY" ]]; then
+        zle autosuggest-accept
+    elif [[ -n "${widgets[history-substring-search-up]}" ]]; then
+        zle history-substring-search-up
+    else
+        zle up-line-or-history
+    fi
+}
+zle -N smart-k-up
+
+function smart-j-down() {
+    if [[ -n "${widgets[history-substring-search-down]}" ]]; then
+        zle history-substring-search-down
+    else
+        zle down-line-or-history
+    fi
+}
+zle -N smart-j-down
+
+# --- Binds ---
+bindkey -M viins '^K' smart-k-up
+bindkey -M vicmd '^K' smart-k-up
+bindkey -M viins '^J' smart-j-down
+bindkey -M vicmd '^J' smart-j-down
+
+# ctrl J & K for going up and down in prev commands
+bindkey -M vicmd 'k' smart-k-up
+bindkey -M vicmd 'j' smart-j-down
 
 # prepend sudo on the current commmand
 bindkey -M emacs '' _sudo_command_line
 bindkey -M vicmd '' _sudo_command_line
 bindkey -M viins '' _sudo_command_line
 
-# fix backspace and other stuff in vi-mode
-bindkey -M viins '\e/' _vi_search_fix
-#bindkey "^?" backward-delete-char
-#bindkey "^H" backward-delete-char
+bindkey '^[[H' beginning-of-line
+bindkey '^[[F' end-of-line
+bindkey -s '^o' '_smooth_fzf^M'
+
 bindkey "^U" backward-kill-line
-# binds
 bindkey "^a" beginning-of-line
 bindkey "^e" end-of-line
-bindkey "^k" kill-line
-bindkey "^j" backward-word
-bindkey "^k" forward-word
 bindkey "^H" backward-kill-word
-# ctrl J & K for going up and down in prev commands
-bindkey "^J" history-search-forward
-bindkey "^K" history-search-backward
 bindkey '^R' fzf-history-widget
+
+#bindkey "^?" backward-delete-char
+#bindkey "^H" backward-delete-char
+#bindkey "^k" kill-line
+#bindkey "^j" backward-word
+#bindkey "^k" forward-word
+#bindkey "^J" history-search-forward
+#bindkey "^K" history-search-backward
+
+bindkey -s '^K' 'ls^M'
+
+# fix backspace and other stuff in vi-mode
+bindkey -M viins '\e/' _vi_search_fix
 
 # Add text object extension -- eg ci" da(:
 autoload -U select-quoted
